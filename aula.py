b0b8bdf7f5673ee4896ee635ce833993c7236562
PhzinXIS <phsouzaferreira2008@gmail.com>
2025-04-25 16:13:19 -0300

    Add files via upload

---
+++ aula.py
@@ -0,0 +1,22 @@
+#print("ola")
+import pyautogui as robo
+
+robo.moveTo(650,1050)
+robo.leftClick(650,1050)
+robo.write("chrome")
+robo.press("enter")
+
+robo.leftClick(650,50)
+robo.write("youtube.com")
+robo.press("enter")
+
+
+robo.moveTo(700,160)
+robo.sleep(3)
+robo.leftClick(700,160)
+robo.write("Dona Cecilia 1")
+robo.press("enter")
+
+robo.moveTo(700,320)
+robo.sleep(3)
+robo.leftClick(700,320)
